34b80cd564d72bf6704256b95a7ff2da7e5ec077
sarit <saritaxelrod@gmail.com>
2021-03-21 13:41:30 +0200

    initial commit

---
+++ HelloGit.java
@@ -0,0 +1,16 @@
+package git;
+import java.util.Scanner;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+		Scanner username = new Scanner(System.in);
+		System.out.println("What is your username?");
+		String name = username.nextLine();
+		System.out.println("Hello " + name);
+
+	}
+
+}
